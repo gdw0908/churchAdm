9dd4dc490760d94aba19f83d44aef89d28538373
gdw0908 <gdw0908@nate.com>
2022-01-25 00:18:28 +0900

    페이징처리 수정

--- client/src/components/Pagination.vue
+++ client/src/components/Pagination.vue
@@ -1,17 +1,15 @@
 <template>
   <nav aria-label="Page navigation example">
     <ul class="pagination justify-content-center">
-      <li :class="['page-item', pageNum === 0 ? 'disabled' : '']">
+      <li :class="['page-item', pageNum === 1 ? 'disabled' : '']">
         <a class="page-link" @click="prevPage" aria-label="Previous">
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
-      <li :class="['page-item', pageNum == i-1 ? 'active' : '']" :key="i" v-for="(i) in pageCount">
-        <a class="page-link" @click="pageNum=i-1">{{i}}</a>
+      <li :class="['page-item', pageNum == i ? 'active' : '']" :key="i" v-for="(i) in range(pageInfo.startIndex, pageInfo.endIndex)">
+        <a class="page-link" @click="pageNum=i">{{i}}</a>
       </li>
-      <!--<li class="page-item"><a class="page-link" href="#">2</a></li>
-      <li class="page-item"><a class="page-link" href="#">3</a></li> -->
-      <li :class="['page-item', pageNum >= pageCount - 1 ? 'disabled' : '']">
+      <li :class="['page-item', pageInfo.last == null ? 'disabled' : '']">
         <a class="page-link" @click="nextPage" aria-label="Next">
           <span aria-hidden="true">&raquo;</span>
         </a>
@@ -24,7 +22,7 @@ export default {
   name: 'PageComponent',
   data () {
     return {
-      pageNum: 0
+      pageNum: 1
     }
   },
   props: {
@@ -36,6 +34,11 @@ export default {
     totalCount: {
       type: String,
       required: true
+    },
+    block: {
+      type: Number,
+      required: false,
+      default: 10 
     }
   },
   methods: {
@@ -44,27 +47,41 @@ export default {
     },
     prevPage () {
       this.pageNum -= 1
+    },
+    range: function (start, end){
+      let list = []
+      for (let i = start; i <= end ; i ++) list.push(i);
+      return list;
     }
   },
   computed: {
-    pageCount () {
-      console.log('this.totalCount==' + this.totalCount)
-      const listLeng = this.totalCount
-      const listSize = this.pageSize
-      let page = Math.floor(listLeng / listSize)
-      if (listLeng % listSize > 0) page += 1
-      /*
-      아니면 page = Math.floor((listLeng - 1) / listSize) + 1;
-      이런식으로 if 문 없이 고칠 수도 있다!
-      */
-      console.log('this.pageNum===' + this.pageNum)
-      const start = this.pageNum * this.pageSize
+    pageInfo () {
+      const totalPage = Math.ceil(this.totalCount / this.pageSize)
+      let currentPage = this.pageNum //현재페이지번호
+      if(currentPage == 0) currentPage = 1
+      const first =  //첫번째 블럭
+        currentPage > 1 ? parseInt(currentPage, 10) - parseInt(1, 10) : null
+      const last =   //마지막 블럭
+        totalPage !== currentPage
+          ? parseInt(currentPage, 10) + parseInt(1, 10)
+          : null
+      let startIndex = (Math.ceil(currentPage / this.block) - 1) * this.block + 1
+      
+      let endIndex =
+      startIndex + this.block > totalPage ? totalPage : startIndex + this.block - 1      
+      //console.log("startIndex=="+startIndex);
+      //console.log("endIndex==="+endIndex);
+      //console.log("first==="+first);
+      //console.log("last==="+last);
+      
+      const start = (this.pageNum-1) * this.pageSize
       const end = start + this.pageSize
       const pgInfo = []
       pgInfo.push(start)
       pgInfo.push(end)
       this.$emit('paging-list', pgInfo) // start, end 값 부모 페이지에 전달
-      return page
+
+      return {startIndex, endIndex, last}
     }
   }
 }
