522cbde5ef780ebc06c0739908dbbb0292dc6d11
soso2234 <mingi371@naver.com>
2022-02-04 11:08:55 +0900

    수정

--- client/src/views/admin/NoticeList.vue
+++ client/src/views/admin/NoticeList.vue
@@ -43,7 +43,7 @@
                 <tr :key="i" v-for="(notice,i) in noticeList">
                   <td scope="row">{{notice.ROWNUM}}</td>
                   <td class="tit">{{notice.TITLE}}</td>
-                  <td class="w_12">{{notice.REG_NM}}</td>    <!-- 등록자 글을 쓴사람 아이디 = WRITER, 이름 = REG_NM -->
+                  <td class="w_12">{{notice.REG_NM}}</td>
                   <td>{{notice.PUBLIC_YN == 'Y' ? '공개' : '비공개'}}</td>
                   <td class="num">{{notice.REG_DT}}</td>
                   <td class="num">{{notice.MOD_DT}}</td>
